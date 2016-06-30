f928546c126955080b9652244cfd10dfe4c85b3a
Luis Diaz <luisdiaz@luiss-imac.local>
2016-06-30 18:40:09 -0500

    Initial Commit

---
+++ Lab05_NavC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab05_NavC
+//
+//  Created by Luis Diaz on 6/30/16.
+//  Copyright © 2016 Luis Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab05_NavC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lab05_NavC
+//
+//  Created by Luis Diaz on 6/30/16.
+//  Copyright © 2016 Luis Diaz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
